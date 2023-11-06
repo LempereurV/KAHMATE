da339e96450455250646103741013e5719668481
Félix Fourreau <felix.fourreau@eleves.enpc.fr>
2023-11-06 15:16:02 +0100

    Transfert des actions rubgymen vers class action

--- board.py
+++ board.py
@@ -1,9 +1,33 @@
 import numpy as np
-import rugbymen.py
+import rugbymen
+import front
 
 class board :
     def __init__():
-        self.coords = np.zeros((8,11)) # Not yet defined outside 
+        n_case=8*11
+        self.coords = np.zeros(n_case) # Not yet defined outside 
+        i=0
+        #The red player chooses his positions 
+        placement_order={"First Normal Rugbyman":rugbymen.Rugbyman("red"),"Second Normal Rugbyman":rugbymen.Rugbyman("red"),"Strong Rugbyman":rugbymen.Strong_rugbyman("red"),"Hard Rugbyman":rugbymen.Hard_rugbyman("red"),"Smart Rugbyman":rugbymen.Smart_rugbyman("red"),"Fast Rugbyman":rugbymen.Fast_rugbyman("red")}
+        while i<6:
+            print("Choose the position of the "+placement_order.keys(i))
+            pos = front.display_number(front.hitbox)
+            while pos.x >4:
+                pos = front.display_number(front.hitbox)
+                print("The position isn't correct, the red team is suppose to be on the left ")
+            self.coords[pos.x][pos.y] = placement_order[placement_order.keys(i)]
+            i+=1
+        i=0
+        placement_order={"First Normal Rugbyman":rugbymen.Rugbyman("blue"),"Second Normal Rugbyman":rugbymen.Rugbyman("blue"),"Strong Rugbyman":rugbymen.Strong_rugbyman("blue"),"Hard Rugbyman":rugbymen.Hard_rugbyman("blue"),"Smart Rugbyman":rugbymen.Smart_rugbyman("blue"),"Fast Rugbyman":rugbymen.Fast_rugbyman("blue")}
+
+        while i<6:
+            print("Choose the position of the "+placement_order.keys(i))
+            pos = front.display_number(front.hitbox)
+            while pos.x >4:
+                pos = front.display_number(front.hitbox)
+                print("The position isn't correct, the red team is suppose to be on the left ")
+            self.coords[pos.x][pos.y] = placement_order[placement_order.keys(i)]
+            i+=1
 
     def placing_rugbymen(self, rugbyman, x, y):
         self.coords[x][y] = rugbyman
--- rugbymen.py
+++ rugbymen.py
@@ -10,56 +10,6 @@ class Rugbyman:
         self.possesion = False  # True if the player has the ball
         self.active = True  # False if the player is KO
 
-    def moove_up(self):
-        self.posy += 1
-
-    def moove_down(self):
-        self.posy -= 1
-
-    def moove_left(self):
-        self.posx -= 1
-
-    def moove_right(self):
-        self.posx += 1
-
-    def tackle(self, card_self, other, card_other):
-        if (
-            self.active == True
-            and self.possesion == False
-            and self.posx == other.posx
-            and self.posy == other.posy
-        ):
-            if (self.attack_bonus + card_self.point) > (
-                other.defense_bonus + card_other.point
-            ):
-                other.active = False
-                return True
-        return False
-
-    def push_through(self, card_self, other, card_other):
-        if (
-            self.active == True
-            and self.possesion == True
-            and self.posx == other.posx
-            and self.posy == other.posy
-        ):
-            if (self.attack_bonus + card_self.point) > (
-                other.defense_bonus + card_other.point
-            ):
-                other.active = False
-                return True
-        return False
-
-    def grubber_kick(self, throw):
-        if self.possesion == True:
-            return True
-        else:
-            return False
-
-    def place_rugbyman(self, x, y):
-        self.posx = x
-        self.posy = y
-
 
 class Strong_rugbyman(Rugbyman):
     def __init__(self, attack_bonus, defense_bonus, moove_points):
